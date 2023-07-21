210264fd1f300cc2e2d7db117f377e94b297c0ad
Tanya chisati <tanya.daniel.chisati@gmail.com>
2023-07-21 23:45:42 +0200

    last commit

--- javascript.js
+++ javascript.js
@@ -1,20 +1,39 @@
-import LocomotiveScroll from 'locomotive-scroll';
 
-const locomotiveScroll = new LocomotiveScroll();
+function setActiveLink() {
+  const navLinks = document.querySelectorAll('.nav-link');
+  const currentPath = window.location.pathname;
 
-import LocomotiveScroll from 'locomotive-scroll';
-
-document.addEventListener('DOMContentLoaded', function () {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector('.info'),
-    smooth: true, // Enable smooth scrolling
+  navLinks.forEach(link => {
+    // Check if the link's href matches the current page path
+    if (link.getAttribute('href') === currentPath) {
+      link.classList.add('active'); // Add 'active' class to the link
+    } else {
+      link.classList.remove('active'); // Remove 'active' class from other links
+    }
   });
-});
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('.info'),
-  smooth: true,
-  smoothMobile: false, // Disable smooth scrolling on mobile devices
-  multiplier: 0.5, // Adjust the sensitivity of scroll speed
-  // Add more options as needed
-});
-AOS.init();
+}
+
+// Call setActiveLink() once when the page loads
+document.addEventListener('DOMContentLoaded', setActiveLink);
+
+const paragraph = "a passionate web developer with a love for crafting beautiful and functional websites"
+const typingParagraph = document.getElementById("typing-paragraph");
+
+function typeText() {
+  let charIndex = 0;
+  const typingInterval = 60; // Adjust the typing speed (in milliseconds)
+
+  function typeNextChar() {
+    typingParagraph.textContent += paragraph[charIndex];
+    charIndex++;
+
+    if (charIndex < paragraph.length) {
+      setTimeout(typeNextChar, typingInterval);
+    }
+  }
+
+  typeNextChar();
+}
+
+// Call the function to start the typing effect when the page loads
+document.addEventListener("DOMContentLoaded", typeText);
